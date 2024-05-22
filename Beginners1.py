863f4ec817bbf0e83653b42778e00371b0db3835
Antonio00827 <23054@student.macleans.school.nz>
2024-05-22 16:19:04 +1200

    Create Beginners1.py

---
+++ Beginners1.py
@@ -0,0 +1,21 @@
+#1
+a = int(input("Input minutes"))
+b = a*60
+print(b)
+#2
+c = int(input("Input card number"))
+d = c*2
+print(d)
+#3
+e = int(input("Enter age"))
+f = 365.25*e
+print(f)
+g = (100-e)*365.25
+#4
+h = input("Name")
+i = input("Adjective")
+j = input("Verb")
+k = input("Place")
+l = input("Food")
+m = input("Vehicle")
+print(h + "is a very" + i + "person to be around. They love to " + j + "in" + k + ". Their favourite food is" + l + "which they eat in their" + m)
